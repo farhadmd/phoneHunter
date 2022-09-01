ff06b53093a806cffb25af5fde4759b1ac683e59
Muhammed Farhad <md6farhad@gmail.com>
2022-09-01 15:09:58 +0600

    search bar added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const loadPhones = async () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+const loadPhones = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch (url);
     const data = await res.json();
     displayPhones(data.data);
@@ -7,14 +7,15 @@ const loadPhones = async () => {
 
 const displayPhones = phones => {
     const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = '';
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
             </div>
@@ -22,5 +23,10 @@ const displayPhones = phones => {
         phonesContainer.appendChild(phoneDiv);
     });
 }
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
+})
 
 loadPhones();
