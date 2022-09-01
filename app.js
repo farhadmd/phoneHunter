9017c8c80520c93842cd15d45f6b549bef2f9072
Muhammed Farhad <md6farhad@gmail.com>
2022-09-01 12:58:45 +0600

    structure create

---
+++ app.js
@@ -0,0 +1,26 @@
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch (url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones => {
+    const phonesContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+    });
+}
+
+loadPhones();
